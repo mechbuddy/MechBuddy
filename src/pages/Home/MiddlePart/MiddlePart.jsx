238788bafcdbaebc950fb5a8145fd942daf79cb0
PaRaDoXx1000 <118244587+PaRaDoXx1000@users.noreply.github.com>
2024-05-31 18:11:18 +0530

    Update MiddlePart.jsx

--- src/pages/Home/MiddlePart/MiddlePart.jsx
+++ src/pages/Home/MiddlePart/MiddlePart.jsx
@@ -7,25 +7,28 @@ const MiddlePart = () => {
   return (
     <div>
       <div className="flex justify-end mt-[2vw] mr-[7vw]">
-        <div className="flex items-center justify-center border-[2px] w-[8vw] p-1 border-[#F31D32] rounded-2xl mr-2 cursor-pointer">
+        <div className="flex items-center justify-center border-[2px] w-fit lg:w-[8vw] p-2 lg:p-1 border-[#F31D32] rounded-2xl mr-2 cursor-pointer">
           <p className="text-[#F31D32] font-[400] mr-[0.5vw]">Whishlist</p>
           <img src={heart} />
         </div>
-        <div className="flex items-center justify-center border-[2px] w-[8vw] p-1 border-[#F31D32] rounded-2xl cursor-pointer">
+        <div className="flex items-center justify-center border-[2px] w-fit lg:w-[8vw] p-2 lg:p-1 border-[#F31D32] rounded-2xl cursor-pointer">
           <p className="text-[#F31D32] font-[400] mr-[0.5vw]">Cart</p>
           <img src={cart} />
         </div>
       </div>
 
-      <div className="flex justify-center items-center mt-[8vh] mb-[8vh]">
-        <input
-          className="w-[35vw] pl-2 h-[8vh] rounded-2xl ml-3 shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
-          type="text"
-          placeholder="Search"
-        ></input>
-        <img className="transform translate-x-[-2vw]" src={search}/>
+      <div className="flex flex-col lg:flex-row justify-center items-center mt-[8vh] mb-[8vh]">
+        <div className="flex justify-center items-center">
+          <input
+            className="w-[62vw] lg:w-[35vw] pl-2 h-[6vh] lg:h-[8vh] rounded-2xl ml-3 shadow-[0_3px_10px_rgb(0,0,0,0.2)]"
+            type="text"
+            placeholder="Search"
+          ></input>
+          <img className="transform translate-x-[-5vw] lg:translate-x-[-2vw]" src={search} />
+        </div>
+        <div className="flex mt-[1vh]">
         <select
-          className=" w-[8vw] h-[8vh] rounded-2xl ml-3 shadow-[0_3px_10px_rgb(0,0,0,0.2)] text-[#5E5E5E]"
+          className="text-sm lg:text-xl lg:w-[8vw] h-[6vh] lg:h-[8vh] rounded-xl ml-3 shadow-[0_3px_10px_rgb(0,0,0,0.2)] text-[#5E5E5E]"
           name="cars"
           id="cars"
         >
@@ -34,20 +37,21 @@ const MiddlePart = () => {
           <option value="mercedes">Mercs</option>
           <option value="audi">Audi</option>
         </select>
-        <div className="flex justify-start items-center w-[10vw] h-[8vh] rounded-2xl ml-3 shadow-[0_3px_10px_rgb(0,0,0,0.2)] text-[#5E5E5E]">
-          <img className="w-[2vw] ml-4" src={tune1} />
-          <p className="ml-4 text-xl">Filter</p>
+        <div className=" flex justify-start items-center lg:w-[10vw] h-[6vh] lg:h-[8vh] rounded-2xl ml-3 shadow-[0_3px_10px_rgb(0,0,0,0.2)] text-[#5E5E5E]">
+          <img className="w-[4vw] lg:w-[2vw] ml-1 lg:ml-4" src={tune1} />
+          <p className=" ml-2 mr-1 lg:ml-4 text-sm lg:text-xl">Filter</p>
         </div>
-        <select className=" text-[#5E5E5E] w-[12vw] h-[8vh] rounded-2xl ml-3 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
+        <select className="text-sm lg:text-xl text-[#5E5E5E]  lg:w-[12vw] h-[6vh] lg:h-[8vh] rounded-2xl ml-3 shadow-[0_3px_10px_rgb(0,0,0,0.2)]">
           <option value="">Customer Review</option>
           <option value="volvo">Volvo</option>
           <option value="saab">Saab</option>
           <option value="mercedes">Mercs</option>
           <option value="audi">Audi</option>
         </select>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default MiddlePart
+export default MiddlePart;
